58d60812cb4c46b146e8405456e3846d1a1fc352
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-02-10 21:06:32 +0700

    refactor: implement resource route instead of basic route

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/transaction', [TransactionController::class, 'index']);
-Route::post('/transaction', [TransactionController::class, 'store']);
-Route::get('/transaction/{id}', [TransactionController::class, 'show']);
-Route::put('/transaction/{id}', [TransactionController::class, 'update']);
-Route::delete('/transaction/{id}', [TransactionController::class, 'destroy']);
+Route::resource('/transaction', TransactionController::class)->except(['create', 'edit']);
